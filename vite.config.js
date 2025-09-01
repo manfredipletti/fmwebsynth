e4149ef889d9d7b665f984451c2f4ff60d1cdbee
Manfredi Pletti <manfredipletti@gmail.com>
2025-09-01 14:00:15 +0200

    ADD PRESET

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,3 @@
-// import { defineConfig } from 'vite'
-// import { resolve } from 'path'
-
-// export default defineConfig({
-//   root: '.',
-//   build: {
-//     outDir: 'dist',
-//     assetsDir: 'assets',
-//     sourcemap: true,
-//     rollupOptions: {
-//       input: {
-//         main: './index.html'
-//       }
-//     }
-//   },
-//   server: {
-//     port: 3000,
-//     open: true,
-//     host: true
-//   },
-//   preview: {
-//     port: 4173,
-//     open: true
-//   },
-//   resolve: {
-//     alias: {
-//       '@': resolve(__dirname, 'js'),
-//       '@models': resolve(__dirname, 'js/models'),
-//       '@views': resolve(__dirname, 'js/views'),
-//       '@controllers': resolve(__dirname, 'js/controllers')
-//     }
-//   }
-// }) 
-
 import { defineConfig } from 'vite'
 
 export default defineConfig({
